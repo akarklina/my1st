61a14ecd0c9a16eb30c937944cc03f22ebfe851e
Belén Fernández <b.fernandez0320221@arts.ac.uk>
2024-12-10 15:42:49 +0000

    Update sketch.js

--- p5 /sketch.js
+++ p5 /sketch.js
@@ -8,6 +8,7 @@ function draw() {
   fill(0, 102, 255); // Set fill color for the circle (blue)
   stroke(0); // Set the outline color (black)
   strokeWeight(2); // Set the thickness of the outline
-
+  
   ellipse(200, 200, 100, 100); // Draw a circle at (200, 200) with a diameter of 100
+  Text("Hello Anita", 200, 200); 
 }
